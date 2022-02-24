5d9532e6927efe7b55cb3a1df8af3890bb69ddc0
4otka <pocakeotakedjkewow@mail.ru>
2022-02-24 13:47:00 +0300

    added morse

--- src/index.js
+++ src/index.js
@@ -38,9 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let arr = [];
+    let arr_element = '';
+    let result = '';
+    let arr1 = ''
+    arr = expr.match( /.{1,10}/g );
 
+    arr.forEach((e) => {
+        e.replace(/^0+/g, '');
+        let i = 0;
+        while (i < e.length){
+            if (e[i] === '1' && e[i+1] === '1') {
+                arr_element += '-'
+            } else if (e[i] === '1' && e[i+1] === '0') {
+                arr_element += '.'
+            } else if (e[i] === '*' ) {
+                arr_element += ' ';
+                i = 10;
+            }
+            i++;
+            i++;
+        }
+        if (arr_element === ' ') {
+            arr1 += ' ';
+            arr_element= '';
+        }
+        for (let key in MORSE_TABLE) {
+            if (key === arr_element) {
+                result = result + MORSE_TABLE[key];
+                arr_element= '';
+            }
+        }
+        arr1 += result;
+        result = '';
+    });
+    return arr1;
+}
 module.exports = {
     decode
 }
